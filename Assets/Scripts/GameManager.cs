36ea6e8d42776f219764ff158b1847c58ef1ede4
Pham Quang Thien <thienpq88@gmail.com>
2017-04-29 18:32:35 +0700

    1st

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,108 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;       //Allows us to use Lists. 
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+	public float levelStartDelay = 2f;
+	public float turnDelay = .1f;
+	public static GameManager instance = null;              //Static instance of GameManager which allows it to be accessed by any other script.
+	private BoardManager boardScript;                       //Store a reference to our BoardManager which will set up the level.
+	public int playerFoodPoints = 100;
+	[HideInInspector] public bool playersTurn = true;
+
+	private UnityEngine.UI.Text levelText;
+	private GameObject levelImage;
+	private int level = 1;                                  //Current level number, expressed in game as "Day 1".
+	private List<Enemy> enemies;
+	private bool enemiesMoving;
+	private bool doingSetup;
+
+	//Awake is always called before any Start functions
+	void Awake()
+	{
+		if (instance == null)
+			instance = this;
+		else if (instance != this)
+			Destroy(gameObject);
+		
+		//Sets this to not be destroyed when reloading scene
+		DontDestroyOnLoad(gameObject);
+		enemies = new List<Enemy> ();
+		boardScript = GetComponent<BoardManager>();
+		InitGame();
+	}
+
+	//this is called only once, and the paramter tell it to be called only after the scene was loaded
+	//(otherwise, our Scene Load callback would be called the very first load, and we don't want that)
+	[RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
+	static public void CallbackInitialization()
+	{
+		//register the callback to be called everytime the scene is loaded
+		SceneManager.sceneLoaded += OnSceneLoaded;
+	}
+
+	//This is called each time a scene is loaded.
+	static private void OnSceneLoaded(Scene arg0, LoadSceneMode arg1)
+	{
+		instance.level++;
+		instance.InitGame ();
+	}
+	
+	//Initializes the game for each level.
+	void InitGame()
+	{
+		doingSetup = true;
+		levelImage = GameObject.Find ("LevelImage");
+		levelText = GameObject.Find ("LevelText").GetComponent<Text>();
+		levelText.text = "Day " + level;
+		levelImage.SetActive (true);
+		Invoke ("HideLevelImage", levelStartDelay);
+
+		enemies.Clear ();
+		boardScript.SetupScene(level);	
+	}
+
+	private void HideLevelImage()
+	{
+		levelImage.SetActive (false);
+		doingSetup = false;
+	}
+
+	public void GameOver()
+	{
+		levelText.text = "After " + level + " days, you starved.";
+		levelImage.SetActive (true);
+		enabled = false;
+	}	
+	
+	//Update is called every frame.
+	void Update()
+	{
+		if (playersTurn || enemiesMoving || doingSetup)
+			return;
+		StartCoroutine(MoveEnemies ());
+	}
+
+	public void AddEnemyToList(Enemy script)
+	{
+		enemies.Add (script);
+	}
+
+	System.Collections.IEnumerator MoveEnemies()
+	{
+		enemiesMoving = true;
+		yield return new WaitForSeconds (turnDelay);
+		if (enemies.Count == 0) {
+			yield return new WaitForSeconds (turnDelay);
+		}
+		for (int i = 0; i < enemies.Count; i++) {
+			enemies [i].MoveEnemy ();
+			yield return new WaitForSeconds (enemies [i].moveTime);
+		}
+		playersTurn = true;
+		enemiesMoving = false;
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,139 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;      //Allows us to use SceneManager
+
+//Player inherits from MovingObject, our base class for objects that can move, Enemy also inherits from this.
+public class Player : MovingObject
+{
+	public float restartLevelDelay = 1f;        //Delay time in seconds to restart level.
+	public int pointsPerFood = 10;              //Number of points to add to player food points when picking up a food object.
+	public int pointsPerSoda = 20;              //Number of points to add to player food points when picking up a soda object.
+	public int wallDamage = 1;                  //How much damage a player does to a wall when chopping it.
+	public UnityEngine.UI.Text foodText;
+
+	private Animator animator;                  //Used to store a reference to the Player's animator component.
+	private int food;                           //Used to store player food points total during level.
+
+	//Start overrides the Start function of MovingObject
+	protected override void Start ()
+	{
+		animator = GetComponent<Animator>();
+		food = GameManager.instance.playerFoodPoints;
+		foodText.text = "Food: " + food;
+		base.Start ();
+	}
+
+	private void OnDisable ()
+	{
+		GameManager.instance.playerFoodPoints = food;
+	}
+
+	private void Update ()
+	{
+		//If it's not the player's turn, exit the function.
+		if(!GameManager.instance.playersTurn) return;
+
+		int horizontal = 0;     //Used to store the horizontal move direction.
+		int vertical = 0;       //Used to store the vertical move direction.
+
+
+		//Get input from the input manager, round it to an integer and store in horizontal to set x axis move direction
+		horizontal = (int) (Input.GetAxisRaw ("Horizontal"));
+
+		//Get input from the input manager, round it to an integer and store in vertical to set y axis move direction
+		vertical = (int) (Input.GetAxisRaw ("Vertical"));
+
+		//Check if moving horizontally, if so set vertical to zero.
+		if(horizontal != 0)
+		{
+			vertical = 0;
+		}
+
+		//Check if we have a non-zero value for horizontal or vertical
+		if(horizontal != 0 || vertical != 0)
+		{
+			//Call AttemptMove passing in the generic parameter Wall, since that is what Player may interact with if they encounter one (by attacking it)
+			//Pass in horizontal and vertical as parameters to specify the direction to move Player in.
+			AttemptMove<Wall> (horizontal, vertical);
+		}
+	}
+
+	//AttemptMove overrides the AttemptMove function in the base class MovingObject
+	//AttemptMove takes a generic parameter T which for Player will be of the type Wall, it also takes integers for x and y direction to move in.
+	protected override void AttemptMove <T> (int xDir, int yDir)
+	{
+		//Every time player moves, subtract from food points total.
+		food--;
+		foodText.text = "Food: " + food;
+
+		//Call the AttemptMove method of the base class, passing in the component T (in this case Wall) and x and y direction to move.
+		base.AttemptMove <T> (xDir, yDir);
+
+		//Hit allows us to reference the result of the Linecast done in Move.
+		RaycastHit2D hit;
+
+		//If Move returns true, meaning Player was able to move into an empty space.
+		if (Move (xDir, yDir, out hit)) 
+		{
+			//Call RandomizeSfx of SoundManager to play the move sound, passing in two audio clips to choose from.
+		}
+
+		//Since the player has moved and lost food points, check if the game has ended.
+		CheckIfGameOver ();
+
+		//Set the playersTurn boolean of GameManager to false now that players turn is over.
+		GameManager.instance.playersTurn = false;
+	}
+
+
+	//It takes a generic parameter T which in the case of Player is a Wall which the player can attack and destroy.
+	protected override void OnCantMove <T> (T component)
+	{
+		Wall hitWall = component as Wall;
+		hitWall.DamageWall (wallDamage);
+		animator.SetTrigger ("PLayerChop");
+	}
+
+	//OnTriggerEnter2D is sent when another object enters a trigger collider attached to this object (2D physics only).
+	private void OnTriggerEnter2D (Collider2D other)
+	{
+		if(other.tag == "Exit")
+		{
+			Invoke ("Restart", restartLevelDelay);
+			enabled = false;
+		}
+		else if(other.tag == "Food")
+		{
+			food += pointsPerFood;
+			foodText.text = "+" + pointsPerFood + " Food: " + food;
+			other.gameObject.SetActive (false);
+		}
+		else if(other.tag == "Soda")
+		{
+			food += pointsPerSoda;
+			foodText.text = "+" + pointsPerSoda + " Food: " + food;
+			other.gameObject.SetActive (false);
+		}
+	}
+
+	private void Restart ()
+	{
+		SceneManager.LoadScene (0);
+	}
+
+	public void LoseFood (int loss)
+	{
+		animator.SetTrigger ("playerHit");
+		food -= loss;
+		foodText.text = "-" + loss + " Food: " + food;
+		CheckIfGameOver ();
+	}
+
+	private void CheckIfGameOver ()
+	{
+		if (food <= 0) 
+		{
+			GameManager.instance.GameOver ();
+		}
+	}
+}
